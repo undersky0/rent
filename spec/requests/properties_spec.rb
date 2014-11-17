7e9f3fd042368dda65d72b702a3941b04e80246a
undersky0 <richardlonesteen@gmail.com>
2014-11-17 15:17:46 +0000

    t

--- spec/requests/properties_spec.rb
+++ spec/requests/properties_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe "Properties", :type => :request do
       expect(response).to have_http_status(200)
     end
   end
+  
+  describe "zoopla API" do 
+    it 'gets a list of nearby properties' do
+      postcode = "CF24+1PL"
+      response = HTTParty.get("http://api.zoopla.co.uk/api/v1/property_listings.json?postcode=#{postcode}&radius=10&listing_status=rent&api_key=xu9atf63rq2rcc9gmmuqunwh")
+      json = response.parsed_response
+      (expect(response.code).to eq(200))
+    end
+  end
 end
